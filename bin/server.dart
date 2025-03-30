0c9aee02bb6056a603fb845aeb892e22a267d523
Kaio C. de Oliveira <hello@kaio.dev>
2025-03-30 20:26:37 -0300

    feat: integrate ShelfRouterAdapter

--- bin/server.dart
+++ bin/server.dart
@@ -1,32 +1,23 @@
 import 'dart:io';
 
+import 'package:financy_api/financy_api.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
-
-// Configure routes.
-final _router =
-    Router()
-      ..get('/', _rootHandler)
-      ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
-}
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
+  final routerAdapter = ShelfRouterAdapter();
+  Routes.configure<Router>(routerAdapter);
+
+  final router = routerAdapter.build();
+
   // Configure a pipeline that logs requests.
   final handler = Pipeline()
       .addMiddleware(logRequests())
-      .addHandler(_router.call);
+      .addHandler(router.call);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
